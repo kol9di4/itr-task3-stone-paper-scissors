e93a34fdd8f0d083489da16ccdd503a397f87cb7
kol9di4 <76259753+kol9di4@users.noreply.github.com>
2024-04-17 19:59:37 +0300

    variable nameing fix

--- Game/KeyGenerator.php
+++ Game/KeyGenerator.php
@@ -8,8 +8,8 @@ use Contracts\IKeyGenerator;
 
 class KeyGenerator implements IKeyGenerator{
 
-    public static function generate(int $bite) : string{
-        $key = bin2hex(random_bytes($bite/8));
+    public static function generate(int $bit) : string{
+        $key = bin2hex(random_bytes($bit/8));
         return $key;
     }
 
